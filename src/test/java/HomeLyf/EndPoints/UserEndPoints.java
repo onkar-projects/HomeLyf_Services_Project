26959f3d79037fe860d80bd0ab1b862996b88b8f
onkar-projects <kure.onkar41@gmail.com>
2024-03-29 20:05:00 +0530

    Reset password

--- src/test/java/HomeLyf/EndPoints/UserEndPoints.java
+++ src/test/java/HomeLyf/EndPoints/UserEndPoints.java
@@ -2,8 +2,12 @@ package HomeLyf.EndPoints;
 
 import static io.restassured.RestAssured.*;
 
+import com.fasterxml.jackson.core.util.RequestPayload;
+
+import HomeLyf.Payload.RestPass_Payload;
 import HomeLyf.Payload.SignUP_Payload;
 import HomeLyf.Payload.UserLogin_Payload;
+import HomeLyf.Payload.userSendEmailOtp_Payload;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -28,6 +32,28 @@ public class UserEndPoints {
 		
 		return response;
 	}
+	
+	public static Response userSendEmailOtp(userSendEmailOtp_Payload Payload) {
+		Response response = given()
+		.contentType(ContentType.JSON)
+		.body(Payload)
+		.log().all()
+		.when().post(Routes.acccount_emailOTP);
+		
+		return response;
+	}
+	
+	public static Response resetPass(RestPass_Payload Payload) {
+		Response response = given()
+		.contentType(ContentType.JSON)
+		.body(Payload)
+		.log().all()
+		.when().post(Routes.acccount_emailOTP);
+		
+		return response;
+	}
+
+	
 
 
 }
